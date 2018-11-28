33fc93fa3323e29dad4d6f777f3a514b8f3ad946
jmilcent <johan.milcent@gmail.com>
2018-11-28 14:29:25 +0100

    add read api rest

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,3 +13,15 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_a_product_json(self):
+        response = self.client.get("/api/v1/products/2")
+        product = response.json
+        self.assertIsInstance(product, dict)
+        self.assertEqual(len(product), 2) # 2 is not a mistake here.
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_not_found(self):
+        response = self.client.get("/api/v1/products/6")
+        self.assertEqual(response.status_code, 404)
+
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,14 @@
 # wsgi.py
 from flask import Flask
 from flask import jsonify
+from flask import abort
 app = Flask(__name__)
 
+PRODUCTS = [
+    {'id': 1, 'name': 'Skello'},
+    {'id': 2, 'name': 'Socialive.tv'},
+    {'id': 3, 'name': 'toto'}
+]
 
 @app.route('/')
 def hello():
@@ -11,10 +17,18 @@ def hello():
 
 @app.route('/api/v1/products')
 def get_products():
-    PRODUCTS = [
-        {'id': 1, 'name': 'Skello'},
-        {'id': 2, 'name': 'Socialive.tv'},
-        {'id': 3, 'name': 'toto'}
-    ]
-
     return jsonify(PRODUCTS)
+
+
+@app.route('/api/v1/products/<int:id>')
+def get_product(id):
+    for product in PRODUCTS:
+        if product['id'] == id:
+            return jsonify(product)
+
+    return abort(404)
+
+
+
+
+

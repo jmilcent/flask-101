317e9b74470898050106a8b059ca58a5598e6fce
jmilcent <johan.milcent@gmail.com>
2018-11-28 11:36:09 +0100

    First endpoint product

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,19 @@
 # wsgi.py
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World! V2"
+    return "Hello World!"
+
+
+@app.route('/api/v1/products')
+def get_products():
+    PRODUCTS = [
+        {'id': 1, 'name': 'Skello'},
+        {'id': 2, 'name': 'Socialive.tv'}
+    ]
+
+    return jsonify(PRODUCTS)

eeaff58cfdb4d1697c21e26f2c2a3dfff4157598
jmilcent <johan.milcent@gmail.com>
2018-11-28 15:22:52 +0100

    implement delete api rest

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,7 +12,7 @@ class TestViews(TestCase):
         response = self.client.get("/api/v1/products")
         products = response.json
         self.assertIsInstance(products, list)
-        self.assertGreater(len(products), 2) # 2 is not a mistake here.
+        self.assertGreater(len(products), 1) # 2 is not a mistake here.
 
     def test_a_product_json(self):
         response = self.client.get("/api/v1/products/2")
@@ -24,4 +24,10 @@ class TestViews(TestCase):
     def test_result_not_found(self):
         response = self.client.get("/api/v1/products/6")
         self.assertEqual(response.status_code, 404)
+
+    def test_del_product(self):
+        response = self.client.delete("/api/v1/products/3")
+        product = response.json
+        self.assertEqual(product, 204)
+
 
